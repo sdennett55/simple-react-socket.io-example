1b7c4c6a965004480eec79f86395c1300d71db48
sdennett55@gmail.com <sdennett55@gmail.com>
2020-07-18 17:03:51 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import io from 'socket.io-client'; 
+
+// storing socket connection in this global variable
+let socket = null;
+
+function handleClick() {
+  // we emit this event that increments the count on the server
+  // and the updated count is emitted back via 'counter updated'
+  socket.emit('counter clicked');
+}
+
+function App() {
+  const [count, setCount] = useState(0); 
+
+  // after component mount...
+  useEffect(() => {
+    // connect to the socket server
+    socket = io('ws://localhost:3001');
+
+    // when connected, look for when the server emits the updated count
+    socket.on('counter updated', function(countFromServer) {
+      // set the new count on the client
+      setCount(countFromServer);
+    })
+    
+  }, []);
+  return (
+    <button onClick={handleClick}>Counter: {count}</button>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,17 @@
+const io = require('socket.io')(3001);
+
+// the count state
+let count = 0;
+
+io.on('connect', function(socket) {
+  // emit to the newly connected client the existing count 
+  socket.emit('counter updated', count);
+
+  // we listen for this event from the clients
+  socket.on('counter clicked', () => {
+    // increment the count
+    count++;
+    // emit to EVERYONE the updated count
+    io.emit('counter updated', count);
+  });
+});
