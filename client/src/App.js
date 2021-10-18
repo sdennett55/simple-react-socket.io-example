048c72c046eeb48ce5b5bffbb2ce5f3e261e1f75
wkrzywiec <wojciechkrzywiec@gmail.com>
2021-10-18 09:15:35 +0200

    react-stomp example

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,80 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client'; 
-
-// storing socket connection in this global variable
-let socket = null;
-
-function handleClick() {
-  // we emit this event that increments the count on the server
-  // and the updated count is emitted back via 'counter updated'
-  socket.emit('counter clicked');
-}
+import SockJsClient from 'react-stomp';
 
 function App() {
-  const [count, setCount] = useState(0); 
 
-  // after component mount...
+  // React-Stomp library
+
+  let clientRef = null
+
+  const handleMsgSTOMP = (msg, topic)=> {
+    console.log('STOMP: \t\t' + msg.time + "\t" + msg.text)
+  }
+
+  const handleDisconnectSTOMP = () =>  {
+    console.log("Disconnecting STOMP connection")
+    clientRef.disconnect();
+  }
+
+  const handleReconnectSTOMP = () =>  {
+    console.log("Reconnecting STOMP connection")
+    clientRef.connect();
+  }
+
+  // Socket.io library
+
+  let socket = null
+
   useEffect(() => {
-    // connect to the socket server
-    socket = io('ws://localhost:5000');
-
-    // when connected, look for when the server emits the updated count
-    socket.on('counter updated', function(countFromServer) {
-      // set the new count on the client
-      setCount(countFromServer);
-    })
+        // connect to the socket server
+        socket = io('http://localhost:8081/chat', {
+          transports: ['polling', 'websocket']
+        });
     
-  }, []);
+        socket.on('connect', function () {
+          console.log('The client has connected with the server');
+        });
+    
+        socket.on('chat', function (data) {
+          // console.log(socket)
+          console.log('Socket.io: \t' + data.time, "\t", data.text);
+            
+        });
+        
+      }, []);
+
+  const handleDisconnectSocketIo = () => {
+    console.log("Disconnecting Socket.io connection")
+    socket.disconnect()
+  }
+
+  const handleReconnectSocketIo = () => {
+    console.log("Reconnecting Socket.io connection")
+    socket.connect()
+  }
+
+
   return (
-    <button onClick={handleClick}>Counter: {count}</button>
+    <div>
+      <div>
+        <button onClick={handleDisconnectSTOMP}>Disconnect STOMP</button>
+      </div>
+      <div>
+        <button onClick={handleReconnectSTOMP}>ReConnect STOMP</button>
+      </div>
+      <div>
+        <button onClick={handleDisconnectSocketIo}>Disconnect Socket.io</button>
+      </div>
+      <div>
+        <button onClick={handleReconnectSocketIo}>ReConnect Socket.io</button>
+      </div>
+      <div>
+        <SockJsClient url={ "http://localhost:8080/chat" } topics={["/topic/chat"]} 
+            onMessage={handleMsgSTOMP} ref={ (client) => { clientRef = client }}
+            debug={ false }/>
+      </div>
+    </div>
   );
 }
 
